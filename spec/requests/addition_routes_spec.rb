7ba9e181559eb6e610aa40e5ea05aa57e7aab6f2
jbranchaud <jbranchaud@gmail.com>
2015-01-30 20:31:00 -0600

    Add two more specs to check other valid addition route use cases.

--- spec/requests/addition_routes_spec.rb
+++ spec/requests/addition_routes_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe "AdditionRoutes", :type => :request do
     expect(page).to have_content("4")
   end
 
+  it "should add a positive and negative value" do
+    visit "/2/plus/-2"
+    expect(page).to have_content("0")
+    visit "/-4/plus/2"
+    expect(page).to have_content("-2")
+  end
+
+  it "should add two negative values" do
+    visit "/-5/plus/-5"
+    expect(page).to have_content("-10")
+  end
+
 end
