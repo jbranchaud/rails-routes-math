f4d4ecde55e1b0ef60701fa68bfe44dedbcab13a
jbranchaud <jbranchaud@gmail.com>
2015-02-01 12:59:33 -0600

    Put regex in a variable to dry up the routes a bit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
-  get '/:op1/plus/:op2' => 'application#add', constraints: { :op1 => /[-]?\d+/, :op2 => /[-]?\d+/ }
+  # reusable regex
+  number = /[-]?\d+/
+  number_with_anchors = /\A[-]?\d+\z/
+
+  get '/:op1/plus/:op2' => 'application#add', constraints: { :op1 => number, :op2 => number }
   get '/:op1/plus/:op2' => 'application#bad_request'
 
-  get '/:op1/minus/:op2' => 'application#subtract', constraints: { :op1 => /[-]?\d+/, :op2 => /[-]?\d+/ }
+  get '/:op1/minus/:op2' => 'application#subtract', constraints: { :op1 => number, :op2 => number }
   get '/:op1/minus/:op2' => 'application#bad_request'
 
-  get '/:op1/times/:op2' => 'application#multiply', constraints: { :op1 => /[-]?\d+/, :op2 => /[-]?\d+/ }
+  get '/:op1/times/:op2' => 'application#multiply', constraints: { :op1 => number, :op2 => number }
   get '/:op1/times/:op2' => 'application#bad_request'
 
-  get '/:op1/by/:op2' => 'application#divide', constraints: proc { |req| /\A[-]?\d+\z/ === req.params[:op1] && /\A[-]?\d+\z/ === req.params[:op2] && req.params[:op2].to_i != 0 }
+  get '/:op1/by/:op2' => 'application#divide', constraints: proc { |req| number_with_anchors === req.params[:op1] && number_with_anchors === req.params[:op2] && req.params[:op2].to_i != 0 }
   get '/:op1/by/:op2' => 'application#bad_request'
 
 end
