be8771d21cbc365abe3fa43c09d90f0fc3ecc6fe
jbranchaud <jbranchaud@gmail.com>
2015-02-11 22:01:31 -0600

    Switch over to actually using procs as a rack endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,26 @@ Rails.application.routes.draw do
   number = /[-]?\d+/
   number_with_anchors = /\A[-]?\d+\z/
 
-  get '/:op1/plus/:op2' => 'application#add',
+  get '/:op1/plus/:op2',
     constraints: { :op1 => number, :op2 => number },
-    to: proc { |env| [200, {}, ["#{:op1.to_i + :op2.to_i}"]] }
+    to: proc { |env| [200, {}, ["#{env['action_dispatch.request.path_parameters'][:op1].to_i + env['action_dispatch.request.path_parameters'][:op2].to_i}"]] }
   get '/:op1/plus/:op2' => 'application#bad_request'
 
-  get '/:op1/minus/:op2' => 'application#subtract',
+  get '/:op1/minus/:op2',
     constraints: { :op1 => number, :op2 => number },
-    to: proc { |env| [200, {}, ["#{:op1.to_i - :op2.to_i}"]] }
+    to: proc { |env| [200, {}, ["#{env['action_dispatch.request.path_parameters'][:op1].to_i - env['action_dispatch.request.path_parameters'][:op2].to_i}"]] }
   get '/:op1/minus/:op2' => 'application#bad_request'
 
-  get '/:op1/times/:op2' => 'application#multiply',
+  get '/:op1/times/:op2',
     constraints: { :op1 => number, :op2 => number },
-    to: proc { |env| [200, {}, ["#{:op1.to_i * :op2.to_i}"]] }
+    to: proc { |env| [200, {}, ["#{env['action_dispatch.request.path_parameters'][:op1].to_i * env['action_dispatch.request.path_parameters'][:op2].to_i}"]] }
   get '/:op1/times/:op2' => 'application#bad_request'
 
-  get '/:op1/by/:op2' => 'application#divide',
+  get '/:op1/by/:op2',
     constraints: proc { |req| number_with_anchors === req.params[:op1] && number_with_anchors === req.params[:op2] && req.params[:op2].to_i != 0 },
-    to: proc { |env| [200, {}, ["#{:op1.to_i / :op2.to_i}"]] }
+    to: proc { |env| [200, {}, ["#{env['action_dispatch.request.path_parameters'][:op1].to_i / env['action_dispatch.request.path_parameters'][:op2].to_i}"]] }
   get '/:op1/by/:op2' => 'application#bad_request'
+
+  match '*path', to: 'application#index', via: :GET
 
 end
