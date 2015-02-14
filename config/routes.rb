5afd7c5c56931ed92d7c33315fd1eabe66ef5c6c
jbranchaud <jbranchaud@gmail.com>
2015-02-14 13:04:04 -0600

    Redirect root to catch-all path, no more index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'application#index'
+  root to: redirect { |path_params, req| '/random' }
 
   # reusable regex
   operand = /(plus)|(minus)|(times)|(by)/
